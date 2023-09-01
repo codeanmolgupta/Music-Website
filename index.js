f10190c96998949b86bd0a0c6a0c65632dba2e3a
codeanmolgupta <136639432+codeanmolgupta@users.noreply.github.com>
2023-09-01 08:57:42 +0530

    adding new files.

---
+++ index.js
@@ -0,0 +1,92 @@
+//  Justin Beaber Click
+
+document.addEventListener("DOMContentLoaded", function() {
+    var clickableartistjustinbeaber = document.querySelectorAll(".artist-justin-beaber-main");
+  
+    clickableartistjustinbeaber.forEach(function(element) {
+      element.addEventListener("click", function() {
+        var linkURL = "./justinbeaber/justinbeaber.html";
+        window.location.href = linkURL;
+      });
+    });
+  });
+
+
+ 
+
+    //  Alenwalkar Click
+
+  document.addEventListener("DOMContentLoaded", function() {
+
+    var clickableartistalanwalkar = document.querySelectorAll(".artist-alan-walkar-main");
+  
+    clickableartistalanwalkar.forEach(function(element) {
+      element.addEventListener("click", function() {
+        var linkURL = "./alenwalkar/alenwalkar.html";
+        window.location.href = linkURL;
+      });
+    });
+  });
+
+
+
+//    //  Arjeet-sigh Click
+
+   document.addEventListener("DOMContentLoaded", function() {
+
+    var clickableartistarjeetsigh = document.querySelectorAll(".artist-arjeet-sigh-main");
+  
+    clickableartistarjeetsigh.forEach(function(element) {
+      element.addEventListener("click", function() {
+        var linkURL = "https://furtherart.netlify.app/";
+        window.location.href = linkURL;
+      });
+    });
+  });
+
+
+
+
+   //   Tanishk-Bagchi Click
+
+   document.addEventListener("DOMContentLoaded", function() {
+
+    var clickableartisttanishkbagchi = document.querySelectorAll(".artist-tanishk-bagchi-main");
+  
+    clickableartisttanishkbagchi.forEach(function(element) {
+      element.addEventListener("click", function() {
+        var linkURL = "https://furtherart.netlify.app/";
+        window.location.href = linkURL;
+      });
+    });
+  });
+
+  
+   //  B-Praak Click
+
+   document.addEventListener("DOMContentLoaded", function() {
+
+    var clickableartistbpraak = document.querySelectorAll(".artist-b-praak-main");
+  
+    clickableartistbpraak.forEach(function(element) {
+      element.addEventListener("click", function() {
+        var linkURL = "https://furtherart.netlify.app/";
+        window.location.href = linkURL;
+      });
+    });
+  });
+
+  
+   //  Jubin-Nautiyal Click
+
+   document.addEventListener("DOMContentLoaded", function() {
+
+    var clickableartistjubinnautiyal = document.querySelectorAll(".artist-jubin-nautiyal-main");
+  
+    clickableartistjubinnautiyal.forEach(function(element) {
+      element.addEventListener("click", function() {
+        var linkURL = "https://furtherart.netlify.app/";
+        window.location.href = linkURL;
+      });
+    });
+  });
